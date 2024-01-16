cfe1eafdfaa8b4b62b0f3a589cc505fe6694e636
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2024-01-16 11:31:58 +0100

    Add files via upload

---
+++ Krug.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tocka
+{
+    internal class Krug
+    {
+        private double radius;
+        private Tocka srediste;
+
+        public Krug(double x, double y, double r)
+        {
+            radius = r;
+            srediste = new Tocka(x, y);
+        }
+
+        public bool dodir(Krug K1, Krug K2)
+        {
+            double l = K1.srediste.udaljenost(srediste);
+
+            if (l <= (K2.radius + K1.radius))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tocka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Krug jedan = new Krug(1, 2, 3);
+            Krug dva = new Krug(1, 2, 3);
+
+            Console.WriteLine(jedan.dodir(jedan, dva));
+
+            Console.ReadKey();
+        }
+    }
+}
